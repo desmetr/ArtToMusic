fd04c60fc34fd79e1a9b9bf23bc612ea1e99fda9
desmetr <rafael.de.smet@gmail.com>
2017-02-20 17:26:16 +0100

    research for entropy

--- doc/graphics/entropy.m
+++ doc/graphics/entropy.m
@@ -4,24 +4,26 @@ function H = MyEntropy(x)
 % Assume input image is of uint8
 
 % display the image
-figure, imshow(x)
+%figure, imshow(x)
 
 % get height and width of image
 [Height, Width] = size(x);
 
 % get the histogram of the image in 256 bins
-[m, Binsx] = imhist(x);
-
+[m, Binsx] = imhist(rgb2gray(x));
+Binsx
 % Normalize the counts
 m = m / (Height * Width);
+m
 
 % output the sum of the histogram values
 sprintf('the sum of the histogram values is = %g', sum(m));
 
 % Note check that the sum of the probabilities is one.
 figure, plot(Binsx, m, 'k')
-xlabel('Piwel value'), ylabel('relative count')
+xlabel('Pixel value'), ylabel('relative count')
 
 % in case of error, log of zero => add a very small value to m)
 H = sum(-m.*log2(m + 1e-10));
-sprintf('the entropy of the image is = %g', H);
+H
+%sprintf('the entropy of the image is = %g', H);
