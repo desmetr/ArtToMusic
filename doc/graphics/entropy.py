00d378756a7d1905b4015b08db5abec9b1c2ae30
desmetr <rafael.de.smet@gmail.com>
2017-04-26 16:31:41 +0200

    doc graphical analysis done

--- doc/graphics/entropy.py
+++ doc/graphics/entropy.py
@@ -1,53 +1,42 @@
-class EntropyObject1(object):
-	def __init__(self, value):
-		super(EntropyObject1, self).__init__()
-		self.hasAttribute = True
-		self.value = value
+from PIL import Image
+import numpy as np
+from matplotlib import pyplot as plt
 
-class EntropyObject2(object):
-	def __init__(self, value):
-		super(EntropyObject2, self).__init__()
-		self.hasAttribute = False
-		self.value = value
+def entropy(signal):
+    # function returns entropy of a signal. Signal must be a 1-D numpy array
+    lensig = signal.size
+    symset = list(set(signal))
+    numsym = len(symset)
+    propab = [np.size(signal[signal == i]) / (1.0 * lensig) for i in symset]
+    ent = np.sum([p * np.log2(1.0 / p) for p in propab])
+    return ent
 
-# Calculates the entropy of the given data set for the target attribute.
-def entropy(data, target_attr):
+colorIm = Image.open('../img/blue_red.jpg')
+greyIm = colorIm.convert('L')
+colorIm = np.array(colorIm)
+greyIm = np.array(greyIm)
 
-	val_freq = {}
-	data_entropy = 0.0
+N = 5
+S = greyIm.shape
+E = np.array(greyIm)
+for row in range(S[0]):
+    for col in range(S[1]):
+        Lx = np.max([0, col - N])
+        Ux = np.min([S[1], col + N])
+        Ly = np.max([0,row - N])
+        Uy = np.min([S[0], row + N])
+        region = greyIm[Ly:Uy, Lx:Ux].flatten()
+        E[row, col] = entropy(region)
 
-	# Calculate the frequency of each of the values in the target attr
-	for record in data:
-		if (record[target_attr] in val_freq):
-			val_freq[record[target_attr]] += 1.0
-		else:
-			val_freq[record[target_attr]]  = 1.0
+plt.subplot(1,3,1)
+plt.imshow(colorIm)
 
-	# Calculate the entropy of the data for the target attribute
-	for freq in val_freq.values():
-		data_entropy += (-freq/len(data)) * math.log(freq/len(data), 2) 
+plt.subplot(1,3,2)
+plt.imshow(greyIm, cmap = plt.cm.gray)
 
-	return data_entropy
+plt.subplot(1,3,3)
+plt.imshow(E, cmap = plt.cm.jet)
+plt.xlabel('Entropy in 10x10 neighbourhood')
+plt.colorbar()
 
-def gain(data, attr, target_attr):
-
-	val_freq = {}
-	subset_entropy = 0.0
-
-	# Calculate the frequency of each of the values in the target attribute
-	for record in data:
-		if (record[attr] in val_freq):
-			val_freq[record[attr]] += 1.0
-		else:
-			val_freq[record[attr]]  = 1.0
-
-	# Calculate the sum of the entropy for each subset of records weighted by their probability of occuring in the training set.
-	for val in val_freq.keys():
-		val_prob = val_freq[val] / sum(val_freq.values())
-		data_subset = [record for record in data if record[attr] == val]
-		subset_entropy += val_prob * entropy(data_subset, target_attr)
-
-	# Subtract the entropy of the chosen attribute from the entropy of the whole data set with respect to the target attribute (and return it)
-	return (entropy(data, target_attr) - subset_entropy)
-
-entropy([EntropyObject1(5), EntropyObject1(60), EntropyObject2(5), EntropyObject1(41.2), EntropyObject2(2), EntropyObject2(78.320), EntropyObject2(475)], True)
+plt.show()
